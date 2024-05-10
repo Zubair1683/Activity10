cfd4ebcb28d98812d7343754a55d1fb6cab7bda0
zubair <zubair.rahimi1683@gmail.com>
2024-05-10 17:08:18 +0300

    first commit

---
+++ Activity10_Slider/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#slider").bxSlider({
+        auto: true,
+        pause: 3000,
+        maxSlides: 1,
+        slideWidth: 250,
+        slideMargin: 1,
+        randomStart: true,
+		pagerType: "short"
+    });
+});
